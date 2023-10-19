cec32627dfb5e37464086e55b1f7d8213260258e
sv1nce <sviatoslav.kushnir.work@gmail.com>
2023-10-19 19:04:44 +0300

    Final Fixes

--- routes.py
+++ routes.py
@@ -21,20 +21,20 @@ def add():
     title = request.form.get('title')
     new_todo = ToDo(title=title, is_complete=False)
     
-    db.session.add(new_todo)  # Поправив помилку тут (замість `all` має бути `add`)
+    db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for('home'))
 
 @app.get('/update/<int:todo_id>')
 def update(todo_id):
-    todo = ToDo.query.filter_by(id=todo_id).first()  # Поправив тут (замість `ToDo` має бути `todo`)
+    todo = ToDo.query.filter_by(id=todo_id).first() 
     todo.is_complete = not todo.is_complete
     db.session.commit()
     return redirect(url_for('home'))
 
 @app.get('/delete/<int:todo_id>')
 def delete(todo_id):
-    todo = ToDo.query.filter_by(id=todo_id).first()  # Поправив тут (замість `ToDo` має бути `todo`)
+    todo = ToDo.query.filter_by(id=todo_id).first() 
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for('home'))
